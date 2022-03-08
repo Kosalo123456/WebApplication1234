05f169c8d45a876cf58b7c3822cdc6b3bd05edb9
mev <mev@PC-19-23>
2022-03-08 17:24:27 +0100

    Add project files.

---
+++ WebApplication1234/Controllers/KosaloController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1234.Controllers
+{
+    public class KosaloController : Controller
+    {
+        // GET: Kosalo
+        public ActionResult Index()
+        {
+
+
+            ViewBag Lokacija="Međimursko Veleučilište u Čakovcu"
+            return View();
+        }
+    }
+}
